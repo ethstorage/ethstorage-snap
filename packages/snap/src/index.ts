899434a5dc41774a7dd3031bfc766ac4e6f1d23c
iteyelmp@gmail.com <lmp19890127>
2023-07-07 17:39:09 +0800

    support sign message

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,24 +1,37 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
 import { ethers } from 'ethers';
-// import {
-//   getSessionParams,
-//   getPermissionParams,
-//   getEnabledSessionSig,
-//   getNonceForSessionKey,
-//   encodeTransfer,
-//   isModuleEnabled,
-// } from './utils/execution';
 
-export const getEOAAccount = async (): Promise<string> => {
-  const accounts: any = await ethereum.request({ method: 'eth_accounts' });
-  return accounts[0];
+export type CreateSessionParams = {
+  owner: string;
 };
 
 export type SessionKeyStorage = {
   owner: string;
   sessionKey: string;
   pk: string;
+};
+
+export const SESSION_TX_TYPE = {
+  SessionTransaction: [
+    { type: 'address', name: 'to' },
+    { type: 'uint256', name: 'amount' },
+    { type: 'bytes', name: 'data' },
+    { type: 'uint256', name: 'nonce' },
+  ],
+};
+
+export type SignAuthorizedTx = {
+  to: string;
+  amount: number;
+  data: string;
+  nonce: number;
+};
+
+export type SignTxType = {
+  owner: string;
+  sessionKeyModuleAddress: string;
+  tx: SignAuthorizedTx;
 };
 
 export const getSessionInfo = async () => {
@@ -48,6 +61,23 @@ export const generateKeyPair = () => {
     address: keyPair.address,
     pk: keyPair.privateKey,
   };
+};
+
+export const signData = async (
+  owner: string,
+  sessionKeyModuleAddress: string,
+  authorizedTx: SignAuthorizedTx,
+) => {
+  const sessionInfo: any = await getSessionInfo();
+  if (sessionInfo.owner === owner) {
+    const signer = new ethers.Wallet(sessionInfo.pk);
+    return await signer._signTypedData(
+      { verifyingContract: sessionKeyModuleAddress, chainId: 3334 },
+      SESSION_TX_TYPE,
+      authorizedTx,
+    );
+  }
+  return undefined;
 };
 
 /**
@@ -93,163 +123,26 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
     }
 
     case 'create_session': {
+      const sessionParams = request.params as CreateSessionParams;
       const keyPair = generateKeyPair();
       // const sessionParams = getSessionParams();
-      // const eoa = await getEOAAccount();
       await storeSessionInfo({
-        owner: 'eoa',
+        owner: sessionParams.owner,
         sessionKey: keyPair.address,
         pk: keyPair.pk,
       });
       return {
         sessionKey: keyPair.address,
-        owner: 'eoa',
+        owner: sessionParams.owner,
       };
     }
 
-    // // TODO : Review error
-    // // Late promise received after Snap finished execution. Promise will be dropped.
-    // case 'interact':
-    //   return new Promise((resolve, reject) => {
-    //     console.log('inside interact');
-    //     // TODO: get session transaction from params that eventually comes from UI
-    //
-    //     const relayer = new LocalRelayer(
-    //       getEOAWallet(
-    //         process.env.REACT_APP_PKEY ||
-    //         '3ff26792ed7e1c706357a1565293371f2f479d331e6c718ac5c5445360e2cef8',
-    //       ),
-    //     );
-    //     console.log('local relayer init..');
-    //     console.log(relayer);
-    //     // prepare data or fetch from params
-    //     getEOAAccount().then(async (eoa) => {
-    //       const sessionInfo: any = await getSessionInfo();
-    //       console.log('session info ');
-    //       console.log(sessionInfo);
-    //
-    //       const nonceFromModule = await getNonceForSessionKey(
-    //         sessionInfo.sessionKey,
-    //       );
-    //       console.log('Got nonce from module', nonceFromModule);
-    //       const authorizedTx = {
-    //         // sessionKey: sessionKey,
-    //         to: usdcAddress,
-    //         amount: 0,
-    //         data: encodeTransfer(receiver, '10000000'),
-    //         nonce: nonceFromModule,
-    //       };
-    //       console.log('authorizedTx');
-    //       console.log(authorizedTx);
-    //
-    //       console.log(eoa);
-    //       console.log('eoa');
-    //
-    //       if (sessionInfo.owner === eoa) {
-    //         const signer = new ethers.Wallet(sessionInfo.pk);
-    //         const signature = await getEnabledSessionSig(
-    //           signer,
-    //           config.sessionKeyModule.address,
-    //           authorizedTx,
-    //           Number(wallet.chainId),
-    //         );
-    //
-    //         console.log('got signature');
-    //         console.log(signature);
-    //
-    //         const tx3 = {
-    //           to: config.sessionKeyModule.address,
-    //           value: 0,
-    //           data: iFaceSessionModule.encodeFunctionData(
-    //             'executeTransaction',
-    //             [
-    //               sessionInfo.sessionKey,
-    //               authorizedTx.to,
-    //               authorizedTx.amount,
-    //               authorizedTx.data,
-    //               signature,
-    //             ],
-    //           ),
-    //           chainId: Number(wallet.chainId),
-    //         };
-    //
-    //         console.log('module txn');
-    //         console.log(tx3);
-    //
-    //         const txHash = await relayer.relayAny(tx3);
-    //         console.log(txHash);
-    //         if (txHash) {
-    //           resolve(txHash.hash);
-    //         } else {
-    //           reject(new Error('reject txn failed'));
-    //         }
-    //
-    //         // relayer calls session module contract
-    //       } else {
-    //         reject(new Error('reject txn failed'));
-    //       }
-    //     });
-    //   });
+    case 'interact': {
+      const data = request.params as SignTxType;
+      return await signData(data.owner, data.sessionKeyModuleAddress, data.tx);
+    }
 
     default:
       throw new Error('Method not found.');
   }
 };
-
-// return new Promise((resolve, reject) => {
-//   const keyPair: KeyPair = generateKeyPair();
-//   console.log('keypair..', keyPair.address);
-//   const sessionParams = getSessionParams();
-//   console.log('session params ', sessionParams);
-//   const permissionParams = getPermissionParams(usdcAddress);
-//   console.log('permission params ', permissionParams);
-//
-//   const relayer = new LocalRelayer(
-//     getEOAWallet(
-//       process.env.REACT_APP_PKEY ||
-//       '3ff26792ed7e1c706357a1565293371f2f479d331e6c718ac5c5445360e2cef8',
-//     ),
-//   );
-//   console.log('local relayer init..');
-//   console.log(relayer);
-//   getsmartAccount().then((_smartAccount) => {
-//     _smartAccount.setRelayer(relayer);
-//     console.log('relayer is set');
-//
-//     getEOAAccount()
-//       .then(async (eoa) => {
-//         await storeSessionInfo({
-//           owner: eoa,
-//           sessionKey: keyPair.address,
-//           pk: keyPair.pk,
-//         });
-//       })
-//       .then(async () => {
-//         const tx2 = {
-//           to: config.sessionKeyModule.address,
-//           data: iFaceSessionModule.encodeFunctionData('createSession', [
-//             keyPair.address,
-//             [permissionParams],
-//             sessionParams,
-//           ]),
-//         };
-//         console.log('prepared txn for create session ');
-//         console.log(tx2);
-//         const scwTx = await _smartAccount.createTransaction({
-//           transaction: tx2,
-//         });
-//         console.log('wallet txn crearted ');
-//         console.log(scwTx);
-//         const txHash = await _smartAccount.sendTransaction({
-//           tx: scwTx,
-//           // gasLimit,
-//         });
-//         console.log(txHash);
-//         if (txHash) {
-//           resolve(txHash);
-//         } else {
-//           reject(new Error('reject txn failed'));
-//         }
-//       });
-//   });
-// });
